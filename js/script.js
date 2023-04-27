645214c7d3010ac6b223cedd2733c61200292682
Teacher IT GCS <mromly@great.sch.id>
2023-04-27 22:42:41 +0700

    membuat website atha store

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// Klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
